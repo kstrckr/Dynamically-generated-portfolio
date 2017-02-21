396047e59547769f84ee0d702f12e532cf3f1bbf
Kurt Strecker <ksphotopro@gmail.com>
2017-02-21 07:23:24 -0500

    added comments for clarity

--- app.js
+++ app.js
@@ -1,8 +1,8 @@
-let mainInput = document.getElementById("input01");
+//let mainInput = document.getElementById("input01");
 
 //let clickEvent = () => {alert("Arrow working")};
 
-let color = "gray";
+//let color = "gray";
 
 //generateImageList(24);
 
@@ -18,10 +18,23 @@ let clickEvent = () => {
 }
 */
 
-let createOutput = (i) => {
+
+//sorts galleryArray by file number (01, 02, 03, etc) so I don't have to re-order the images manually when adding new shots
+galleryArray.sort(function(a, b){
 	
+	if(a.substr(-6,2) < b.substr(-6,2)) return -1;
+	if(a.substr(-6,2) > b.substr(-6,2)) return 1;
+	return 0;
+	
+});
+
+console.log(galleryArray);
+
+let createOutput = (i) => {
+	//targets the output div that already exists in index.html
 	let target = document.getElementById("output");
 	
+	//this section generates the divs that will be nested to create each gallery image
 	let bgDiv = document.createElement("div");
 	
 	let numberHolder = document.createElement("div");
@@ -30,35 +43,45 @@ let createOutput = (i) => {
 	
 	let newPhoto = document.createElement("img");
 	
+	//defines the image URL based on file name stored in galleryArray
 	let imgUrl = "imgs/" + galleryArray[i];
 	
+	//pulls the background color from the beginning of the file name of each element in galleryArray
 	let bgColor = "#" + galleryArray[i].substr(0,6);
-		
+	
+	//sets the <img src=
 	newPhoto.setAttribute("src", imgUrl);
 	
+	//sets the background color defined in file name
 	bgDiv.style.background = bgColor;
+		
+	//dynamically creates the h3 content
+	numberHolder.innerHTML = "<h3>" + (i+1) + "/" + galleryArray.length + "</h3>"
 	
+	//this section sets up the class names for styling w/ style.css
 	bgDiv.className = "full-shot";
 	
 	numberHolder.className = "number";
-	
-	numberHolder.innerHTML = "<h3>" + (i+1) + "/" + galleryArray.length + "</h3>"
 	
 	imageHolder.className = "image-holder";
 	
 	newPhoto.className = "image-main";
 	
+	//this section nests the divs in the proper order to create each gallery image w/ background and number
 	bgDiv.appendChild(numberHolder);
 	bgDiv.appendChild(imageHolder);
 	imageHolder.appendChild(newPhoto);
-		
+	
+	//appends the completed bgDiv to the output class in index.html
 	target.appendChild(bgDiv);
 	
+	//exits the recursive createOutput function at end of galleryArray
 	if (i === galleryArray.length-1){
 		//alert(imgUrl);
 		return;
 	};
 	
+	//recurisve counter and call
 	i++
 	
 	createOutput(i);
--- gallery_data.js
+++ gallery_data.js
@@ -1,5 +1,6 @@
-let imageList = [];
+// let imageList = [];
 
+/* 
 let generateImageList = (x) => {
 	
 	imageList.unshift(x);
@@ -10,35 +11,35 @@ let generateImageList = (x) => {
 	
 	generateImageList(x)
 };
-
+*/
 
 
 const galleryArray = [
 	
-	"066243_7.jpg",
-	"142949_5.jpg",
-	"19A2E29_6.jpg",
+	"066243_07.jpg",
+	"142949_05.jpg",
+	"19A2E29_06.jpg",
 	"232730_18.jpg",
 	"3A4954_13.jpg",
 	"46130A_12.jpg",
 	"4B332E_20.jpg",
 	"4B362B_22.jpg",
 	"4E555A_17.jpg",
-	"56191B_8.jpg",
+	"56191B_08.jpg",
 	"673D19_14.jpg",
-	"6C4124_6.jpg",
+	"6C4124_06.jpg",
 	"757980_19.jpg",
-	"757EA9_1.jpg",
+	"757EA9_01.jpg",
 	"868C91_10.jpg",
 	"9A9AA2_21.jpg",
-	"A35037_4.jpg",
+	"A35037_04.jpg",
 	"A36D27_15.jpg",
 	"CFC59B_11.jpg",
 	"EE001C_24.jpg",
 	"EEDAB7_23.jpg",
-	"F3C594_2.jpg",
-	"F4534C_3.jpg",
-	"F99338_9.jpg"
+	"F3C594_02.jpg",
+	"F4534C_03.jpg",
+	"F99338_09.jpg"
 
 	
 ];
